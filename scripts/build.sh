42df2a5f3506c9198602b468c0d0d917d58ba197
shivanandvp <shivanand.pattanshetti@gmail.com>
2022-01-06 04:07:00 -0600

    build: Fix package list and improve build script messages

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,16 +5,23 @@ PROJECT_DIRECTORY="$(dirname -- "$SCRIPT_DIRECTORY")"
 WORK_DIRECTORY=/var/tmp/archiso
 OUTPUT_DIRECTORY="$PROJECT_DIRECTORY"/output
 
-set -o xtrace
-
 # Install prerequisites
+echo ""
 sh "$SCRIPT_DIRECTORY"/setup.sh
 
 # Create directories if they don't exist already
+echo ""
+echo "Creating necessary directories if they do not exist..."
+echo ""
+set -o xtrace
 mkdir -p "$WORK_DIRECTORY"
 mkdir -p "$OUTPUT_DIRECTORY"
+set +o xtrace
 
 # Build the ISO image
-sudo mkarchiso -w "$WORK_DIRECTORY" -o "$OUTPUT_DIRECTORY" "$@" "$PROJECT_DIRECTORY"
-
+echo ""
+echo "Building the ISO image..."
+echo ""
+set -o xtrace
+sudo mkarchiso -v -w "$WORK_DIRECTORY" -o "$OUTPUT_DIRECTORY" "$@" "$PROJECT_DIRECTORY"
 set +o xtrace
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "$0")")"
 PROJECT_DIRECTORY="$(dirname -- "$SCRIPT_DIRECTORY")"
 
+echo "Installing prerequisites if needed. Ignore any warnings..."
+echo ""
 set -o xtrace
 sudo pacman -S --needed "$@" archiso
 set +o xtrace
