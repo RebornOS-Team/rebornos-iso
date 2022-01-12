bb101d51459d45f355aba541c25bfe73684b0cfc
shivanandvp <shivanandvp@rebornos.org>
2022-01-12 13:04:41 -0600

    Add trigger for database refresh when the internet is connected

--- profiledef.sh
+++ profiledef.sh
@@ -18,6 +18,7 @@ file_permissions=(
   ["/etc/gshadow"]="0:0:0400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
+  ["/etc/NetworkManager/dispatcher.d/refresh-repos.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
