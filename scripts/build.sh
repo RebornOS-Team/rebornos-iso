31d390df60bfa83e9801a29cc5e05fcb427af6fa
shivanandvp <shivanandvp.oss@gmail.com>
2022-06-25 10:20:02 -0500

    feat: When repo mkarchiso is too old for git archiso, prompt to automatically download git mkarchiso and use it

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,15 @@ PROJECT_DIRECTORY="$(dirname -- "$SCRIPT_DIRECTORY")"
 WORK_DIRECTORY=/var/tmp/archiso
 OUTPUT_DIRECTORY="$PROJECT_DIRECTORY"/output
 
-# Install prerequisites
+read -p "Do you want to setup prerequisites? [Y/n]: " -n 1 -r
 echo ""
-sh "$SCRIPT_DIRECTORY"/setup_prerequisites.sh
+if [ "$REPLY" == "N" ] || [ "$REPLY" == "n" ]; then
+    :
+else
+    # Install prerequisites
+    echo ""
+    sh "$SCRIPT_DIRECTORY"/setup_prerequisites.sh
+fi
 
 # Clear old directories
 echo ""
@@ -34,4 +40,20 @@ echo ""
 set -o xtrace
 # rsync 
 sudo mkarchiso -v -w "$WORK_DIRECTORY" -o "$OUTPUT_DIRECTORY" "$@" "$PROJECT_DIRECTORY"
+EXIT_CODE="$?"
 set +o xtrace
+
+echo ""
+
+if [ "$EXIT_CODE" -ne 0 ]; then
+    read -p "\"mkarchiso\" failed. Do you want to try with the Git version? [Y/n]: " -n 1 -r
+    echo ""
+    if [ "$REPLY" == "N" ] || [ "$REPLY" == "n" ]; then
+        exit 1
+    else
+        set -o xtrace
+        (cd "$SCRIPT_DIRECTORY" && wget -O "mkarchiso" "https://gitlab.archlinux.org/archlinux/archiso/-/raw/master/archiso/mkarchiso") \
+            && sudo bash "$SCRIPT_DIRECTORY"/mkarchiso -v -w "$WORK_DIRECTORY" -o "$OUTPUT_DIRECTORY" "$@" "$PROJECT_DIRECTORY" # Only works with bash shell
+        set +o xtrace
+    fi
+fi
