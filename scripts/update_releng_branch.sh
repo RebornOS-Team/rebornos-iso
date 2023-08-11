8b612494873d6a07c0e891ad5782309dfc53364d
shivanandvp <shivanandvp.oss@gmail.com>
2023-08-11 02:13:11 -0500

    fix: :bug: Fix the update_releng_branch.sh script

--- scripts/update_releng_branch.sh
+++ scripts/update_releng_branch.sh
@@ -9,21 +9,27 @@ INITIAL_BRANCH="$(git branch --show-current)"
 
 (
     cd "$PROJECT_DIRECTORY" \
-    && { git remote add upstream https://gitlab.archlinux.org/archlinux/archiso || true }
-    && git fetch --all \
-    && git checkout -B _archiso --track upstream \
-    && git reset --hard upstream \
-    && { git branch -D _releng || true }
-    && git switch --orphan _releng \
-    && git checkout _archiso -- configs/releng \
-    && git checkout _archiso -- archiso/mkarchiso \
-    && git checkout main -- .gitignore \
-    && git checkout main -- .gitattributes \
-    && git add configs/releng \
-    && git add archiso/mkarchiso \
-    && git commit -m "Updated from the archiso branch" \
+        && { 
+            git remote add upstream "https://gitlab.archlinux.org/archlinux/archiso" \
+            || true
+        } \
+        && git fetch --all \
+        && git checkout -B _archiso --track upstream \
+        && git reset --hard upstream \
+        && { 
+            git branch -D _releng \
+            || true
+        } \
+        && git switch --orphan _releng \
+        && git checkout _archiso -- configs/releng \
+        && git checkout _archiso -- archiso/mkarchiso \
+        && git checkout main -- .gitignore \
+        && git checkout main -- .gitattributes \
+        && git add configs/releng \
+        && git add archiso/mkarchiso \
+        && git commit -m "Updated from the archiso branch"
 )
 
-git checkout -f "$INITIAL_BRANCH"
+git checkout "$INITIAL_BRANCH"
 
 set +o xtrace
