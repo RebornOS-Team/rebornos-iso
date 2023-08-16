40412c7fbb1b9960d16a2af635e27d710ca51ed0
shivanandvp <shivanandvp.oss@gmail.com>
2023-08-16 11:42:22 -0500

    build: :hammer: Give executable permissions earlier
    Give executable permissions earlier since the script organization changed

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,11 @@
 SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "$0")")"
 PROJECT_DIRECTORY="$(dirname -- "$SCRIPT_DIRECTORY")"
 
+echo ""
+echo "Giving executable permissions to the required scripts..."
+echo ""
+set -o xtrace
+sudo chmod -R +x "$PROJECT_DIRECTORY/scripts"
+set +o xtrace
+
 "$SCRIPT_DIRECTORY/_build_inner.sh" 2>&1 | tee -a "$PROJECT_DIRECTORY/output.log"
