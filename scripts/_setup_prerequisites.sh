6c5854381e21379e7b5e9e1034dc872c2310691e
shivanandvp <shivanandvp.oss@gmail.com>
2023-08-16 11:41:30 -0500

    build: :bug: Avoid partial upgrade for the keyring packages

--- scripts/_setup_prerequisites.sh
+++ scripts/_setup_prerequisites.sh
@@ -13,10 +13,13 @@ echo "Project Directory: $PROJECT_DIRECTORY"
 echo "Script Directory: $SCRIPT_DIRECTORY"
 
 echo ""
-echo "Giving executable permissions to the required scripts..."
+echo "Installing prerequisites if needed. Ignore any warnings..."
 echo ""
 set -o xtrace
-sudo chmod -R +x "$PROJECT_DIRECTORY/scripts"
+sudo pacman -S --noconfirm archlinux-keyring rebornos-keyring
+sudo pacman -S --noconfirm --needed archiso # wget rsync git git-lfs
+# git lfs install
+# git lfs pull
 set +o xtrace
 
 read -t 5 -p "Do you want to skip refreshing mirrors? (The default option will be selected in 5 seconds) [y/N] : " -n 1 -r
@@ -29,16 +32,6 @@ if [ "$REPLY" != "Y" ] && [ "$REPLY" != "y" ]; then
     sudo refresh-mirrors
     set +o xtrace
 fi
-
-echo ""
-echo "Installing prerequisites if needed. Ignore any warnings..."
-echo ""
-set -o xtrace
-sudo pacman -Sy --noconfirm archlinux-keyring rebornos-keyring
-sudo pacman -Sy --noconfirm --needed archiso # wget rsync git git-lfs
-# git lfs install
-# git lfs pull
-set +o xtrace
 
 echo ""
 echo "Copying mirrorlists..."
