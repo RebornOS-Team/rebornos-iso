bf07585e5098e8b9c588e0ffb4a264efaabe1753
shiva-patt-oss <shiva.patt.oss@gmail.com>
2023-10-21 23:53:22 -0500

    fix: :card_file_box: Copy pacman databases too to the ISO

--- scripts/_setup_prerequisites.sh
+++ scripts/_setup_prerequisites.sh
@@ -8,9 +8,21 @@ echo ""
 SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "$0")")"
 PROJECT_DIRECTORY="$(dirname -- "$SCRIPT_DIRECTORY")"
 ISO_CONFIGURATION_DIRECTORY="$PROJECT_DIRECTORY/configs/releng"
+ISO_SYNC_DIRECTORY="$ISO_CONFIGURATION_DIRECTORY/airootfs/var/lib/pacman/sync"
 
 echo "Project Directory: $PROJECT_DIRECTORY"
 echo "Script Directory: $SCRIPT_DIRECTORY"
+
+read -t 5 -p "Do you want to skip upgrading your system? (The default option will be selected in 5 seconds) [y/N] : " -n 1 -r
+echo ""
+if [ "$REPLY" != "Y" ] && [ "$REPLY" != "y" ]; then
+    echo ""
+    echo "Upgrading your system..."
+    echo ""
+    set -o xtrace
+    sudo pacman -Syyu
+    set +o xtrace
+fi
 
 echo ""
 echo "Installing prerequisites if needed. Ignore any warnings..."
@@ -34,9 +46,15 @@ if [ "$REPLY" != "Y" ] && [ "$REPLY" != "y" ]; then
 fi
 
 echo ""
-echo "Copying mirrorlists..."
+echo "Copying mirrorlists and databases..."
 echo ""
 set -o xtrace
 cp -f /etc/pacman.d/reborn-mirrorlist "$ISO_CONFIGURATION_DIRECTORY"/airootfs/etc/pacman.d/
 cp -f /etc/pacman.d/mirrorlist "$ISO_CONFIGURATION_DIRECTORY"/airootfs/etc/pacman.d/
+mkdir -p "$ISO_SYNC_DIRECTORY" \
+    && {
+        cp -f /var/lib/pacman/sync/core.db "$ISO_SYNC_DIRECTORY"/core.db
+        cp -f /var/lib/pacman/sync/core.db "$ISO_SYNC_DIRECTORY"/core.db
+        cp -f /var/lib/pacman/sync/core.db "$ISO_SYNC_DIRECTORY"/core.db
+    }
 set +o xtrace
